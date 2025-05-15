510d85fd2d027c1a15602b5ffa470db192fc6231
LMB-TI <alonso@lmb-industrial.com>
2025-05-15 10:35:25 -0600

    Dando estilos a search

--- app/routes/results.jsx
+++ app/routes/results.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/results.css';
 import { useLoaderData } from '@remix-run/react';
 
 export async function loader({ request, context }) {
@@ -60,18 +61,26 @@ export default function Results() {
     const { term, products } = useLoaderData();
 
     return (
-        <div className="resultado-pagina">
+        <div className="resultado-pagina my-2 mx-5">
             <h1>Resultados para: {term}</h1>
             {products.length === 0 ? (
                 <p>No se encontraron resultados.</p>
             ) : (
-                <div className="lista-productos">
+                <div className="row lista-productos">
                     {products.map((prod) => (
-                        <div key={prod.id} className="producto">
-                            <a href={`/products/${prod.handle}`}>
-                                <img src={prod.featuredImage?.url} alt={prod.featuredImage?.altText} />
-                                <h2>{prod.title}</h2>
-                            </a>
+                        <div key={prod.id} className="col-12 col-sm-6 col-md-4 mb-4">
+                            <div className="card h-100 product-search">
+                                <a href={`/products/${prod.handle}`} className="text-decoration-none text-dark">
+                                    <img
+                                        src={prod.featuredImage?.url}
+                                        alt={prod.featuredImage?.altText}
+                                        className="card-img-top"
+                                    />
+                                    <div className="card-body">
+                                        <h2 className="h6 card-title">{prod.title}</h2>
+                                    </div>
+                                </a>
+                            </div>
                         </div>
                     ))}
                 </div>
